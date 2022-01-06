fe5039658f14446f46be9a96a74ae72678b80b6e
Rakib-RS <rakib.rscse@gmail.com>
2022-01-06 20:04:02 +0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 ooksJson = file_get_contents('books.json');
     $books = json_decode($booksJson, true);
         $id = $_GET['id'];
-        $id = $id;
+        $id = $id -1;
         array_splice($books, $id, 1);
         //echo $books;
         file_put_contents('./books.json',json_encode($books));
